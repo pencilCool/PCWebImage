ec15c1e2029bb8e6601d8b0b9159a8003bdfcb89
pencilCool <yhtang_xt@163.com>
2017-06-02 14:32:17 +0800

    YYWebImageManager complete

--- PCImageCache/YYWebImageManager.swift
+++ PCImageCache/YYWebImageManager.swift
@@ -8,6 +8,16 @@
 
 import Foundation
 
+private var networkIndicatorInfoKey: Void?
+
+struct YYWebImageApplicationNetworkIndicatorInfo {
+    var count: Int = 0
+    var timer: Timer!
+}
+
+
+
+
 
 public struct YYWebImageOptions:OptionSet {
     public let rawValue: Int
@@ -64,6 +74,16 @@ typealias YYWebImageCompletionBlock = (UIImage?, URL, YYWebImageFromType, YYWebI
 
 
 class YYWebImageManager {
+    var cache: YYImageCache
+    var queue: OperationQueue?
+    var sharedTransformBlock: YYWebImageTransformBlock?
+    var timeout: TimeInterval
+    var username: String? = nil
+    var password: String? = nil
+    var headers: [String:String]?
+    var headersFilter :((URL, [String:String]?) -> [String: String])?
+    var cacheKeyFilter :((URL) -> String)? = nil
+    
     public static let shared:YYWebImageManager = {
        let cache = YYImageCache.shared
        var  queue = OperationQueue.current
@@ -72,10 +92,140 @@ class YYWebImageManager {
     }()
     
     init(withCache cache:YYImageCache, queue que:OperationQueue) {
+        self.cache = cache
+        self.queue = que
+        self.timeout = 15.0
+        if YYImageWebPAvailable() {
+            headers = ["Accept" : "image/webp,image/*;q=0.8"]
+        } else {
+            headers = ["Accept" : "image/*;q=0.8" ];
+        }
         
     }
     init() {
         fatalError("YYWebImageManager init error -- Use the designated initializer to init")
        
     }
+    
+    func requestImage(with url:URL,
+                      options opt :YYWebImageOptions,
+                      progress pro:YYWebImageProgressBlock?,
+                      transform trans: YYWebImageTransformBlock?,completion comp: YYWebImageCompletionBlock?) -> YYWebImageOperation {
+        var request = URLRequest(url: url)
+        request.timeoutInterval = self.timeout
+        request.httpShouldHandleCookies = opt.contains(.handleCookies)
+        request.allHTTPHeaderFields = headersFor(URL: url)
+        request.httpShouldUsePipelining = true
+        
+        if opt.contains(.useNSURLCache) {
+             request.cachePolicy =   .useProtocolCachePolicy
+        } else {
+            request.cachePolicy  =    .reloadIgnoringLocalCacheData
+        }
+        
+        let operation = YYWebImageOperation(with: request, options: opt, cache: self.cache, cacheKey: cacheKeyFor(URL: url), progress: pro, transform: trans , completion: comp)
+        
+        if username != nil , password != nil  {
+            operation.credential = URLCredential(user: username!, password: password!, persistence: URLCredential.Persistence.forSession)
+        }
+        
+        if self.queue != nil {
+            self.queue!.addOperation(operation)
+        } else {
+            operation.start()
+        }
+        return operation
+        
+    }
+    
+    func headersFor(URL url:URL) -> [String: String]? {
+        if self.headersFilter != nil {
+            return self.headersFilter!(url, self.headers)
+        } else {
+            return self.headers
+        }
+        
+    }
+    
+    func cacheKeyFor(URL url:URL) -> String {
+        if self.cacheKeyFilter != nil {
+            return self.cacheKeyFilter!(url)
+        } else {
+            return url.absoluteString
+        }
+    }
+   
+
+    
+
+
+     // MARK: - Network Indicator
+    
+
+    static func YYSharedApplication() -> UIApplication? {
+        return  nil
+    }
+
+    static var  networkIndicatorInfo:YYWebImageApplicationNetworkIndicatorInfo? {
+        get {
+            return objc_getAssociatedObject(self, &networkIndicatorInfoKey) as? YYWebImageApplicationNetworkIndicatorInfo
+        }
+        set {
+            objc_setAssociatedObject(self, &networkIndicatorInfoKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
+    
+    @objc static func delaySetActivity(_ timer: Timer) {
+        guard let  app = YYWebImageManager.YYSharedApplication() else {return}
+        let  visiable = timer.userInfo as! Bool
+        if app.isNetworkActivityIndicatorVisible != visiable {
+            app.isNetworkActivityIndicatorVisible = true
+        }
+        timer.invalidate()
+    }
+
+   
+    static func  changeNetworkActivityCount(delta: Int) {
+        guard let  _ = YYWebImageManager.YYSharedApplication() else {
+            return
+        }
+        
+        let block: ()->() = {
+            var  info: YYWebImageApplicationNetworkIndicatorInfo!  =  networkIndicatorInfo;
+            if  info == nil  {
+                info = YYWebImageApplicationNetworkIndicatorInfo();
+                YYWebImageManager.networkIndicatorInfo = info;
+            }
+        var  count = info.count
+        count += delta
+        info.count = count
+        info.timer.invalidate()
+        info.timer = Timer(timeInterval: (1 / 3.0), target: self, selector: #selector(delaySetActivity), userInfo: (info.count > 0), repeats: true)
+           
+        RunLoop.main.add(info.timer, forMode: .commonModes)
+        };
+        if Thread.isMainThread {
+            block()
+        } else {
+            DispatchQueue.main.sync {
+               block()
+            }
+        }
+    }
+    
+    static func incrementNetworkActivityCount() {
+        changeNetworkActivityCount(delta: 1)
+    }
+    
+    static func decrementNetworkActivityCount(){
+        changeNetworkActivityCount(delta: -1)
+    }
+    
+    static func currentNetworkActivityCount() -> Int {
+    let info = networkIndicatorInfo
+    return info!.count;
+    }
+
+    
 }
--- PCImageCache/YYWebImageOperation.swift
+++ PCImageCache/YYWebImageOperation.swift
@@ -7,3 +7,139 @@
 //
 
 import Foundation
+
+public extension DispatchQueue {
+    private static var onceToken = [String]()
+    public class func once(_ token: String, _ block:@escaping () -> Void) {
+        objc_sync_enter(self)
+        defer {
+            objc_sync_exit(self)
+        }
+        if onceToken.contains(token) {
+            return
+        }
+        onceToken.append(token)
+        block()
+    }
+}
+
+
+
+class YYWebImageOperation: Operation {
+    
+    public private(set) var request:URLRequest
+    public private(set) var response:URLResponse?
+    public private(set) var cache: YYImageCache?
+    public private(set) var cacheKey: String
+    public private(set) var options:YYWebImageOptions
+    
+    
+    private var lock:NSRecursiveLock = NSRecursiveLock()
+    private var connection: NSURLConnection!
+    private var data: Data?
+    private var expectedSize: Int = 0
+    private var taskID: UIBackgroundTaskIdentifier = UIBackgroundTaskInvalid
+    
+    private var lastProgressiveDecodeTimestamp: TimeInterval = 0
+    private var progressiveDecoder: YYImageDecoder?
+    private var progressiveIgnored: Bool = false
+    private var progressiveDetected: Bool = false
+    private var progressiveScanedLength: UInt = 0
+    private var progressiveDisplayCount: UInt = 0
+    private var progress: YYWebImageProgressBlock?
+    private var transform: YYWebImageTransformBlock?
+    private var completion: YYWebImageCompletionBlock?
+    
+    
+    private var _executing : Bool = false
+    override var isExecuting : Bool {
+        get { return _executing }
+        set {
+            guard _executing != newValue else { return }
+            willChangeValue(forKey: "isExecuting")
+            _executing = newValue
+            didChangeValue(forKey: "isExecuting")
+        }
+    }
+    
+    
+    private var _finished : Bool = false
+    override var isFinished : Bool {
+        get { return _finished }
+        set {
+            guard _finished != newValue else { return }
+            willChangeValue(forKey: "isFinished")
+            _finished = newValue
+            didChangeValue(forKey: "isFinished")
+        }
+    }
+
+
+    private static let _networkThread:Thread = {
+        let thread = Thread(target: self, selector: #selector(_networkThreadMain), object: nil)
+        return thread
+    }()
+    
+
+    
+    var shouldUseCredentialStorage:Bool = true
+    var credential: URLCredential?
+    
+    
+     init(with request:URLRequest, options:YYWebImageOptions, cache:YYImageCache?,cacheKey: String?,progress:YYWebImageProgressBlock?, transform: YYWebImageTransformBlock?,completion: YYWebImageCompletionBlock?)
+          {
+            
+            self.request = request
+            self.options = options
+            self.cache = cache
+            self.cacheKey = cacheKey ?? request.url!.absoluteString
+            self.progress = progress
+            self.transform = transform
+            self.completion = completion
+            super.init()
+    }
+    
+   
+    
+    @objc private static func _networkThreadMain(object:Any) {
+        autoreleasepool {
+            Thread.current.name = "com.pencilCool.webimage.request"
+            let runLoop = RunLoop.current
+            runLoop.add(NSMachPort.port(withMachPort: 0), forMode: .defaultRunLoopMode)
+        }
+    }
+    
+    static var counter: Int32 = 0
+    var imageQueue:DispatchQueue = {
+        
+        var queueCount = YYWebImageOperation.imageQueues.count;
+        var cur: Int32 = Int32(OSAtomicIncrement32(&YYWebImageOperation.counter));
+        cur = cur > 0 ?  cur : -cur;
+        return YYWebImageOperation.imageQueues[Int(cur) % queueCount];
+    }()
+    
+    static let   imageQueues : [DispatchQueue]  = {
+        let maxQueueCount = 16
+        var queueCount:Int = 0
+        var queues : [DispatchQueue] = []
+        queueCount = ProcessInfo.processInfo.activeProcessorCount
+        queueCount = queueCount < 1 ? 1 : queueCount > maxQueueCount ? maxQueueCount:queueCount
+            if #available(iOS 8.0, *) {
+                for index in 0 ... queueCount {
+                    let queue = DispatchQueue(label: "com.pencilCool.image.decode", qos: .utility)
+                    queues.append(queue)
+                }
+            } else {
+                // 可以不写了
+                let queue = DispatchQueue(label: "com.pencilCool.image.decode", qos: .utility, target:DispatchQueue.global(qos: .utility))
+                queues.append(queue)
+        }
+        return queues
+    }()
+    
+
+}
+
+extension YYWebImageOperation:NSURLConnectionDelegate {
+    
+}
